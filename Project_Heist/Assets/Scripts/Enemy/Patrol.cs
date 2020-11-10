dff8c2ae23f6164cee92096864ad0e05bb29f14d
Shellmadinger <48625443+Shellmadinger@users.noreply.github.com>
2020-11-10 12:40:29 -0500

    Added Navigation

---
+++ Project_Heist/Assets/Scripts/Enemy/Patrol.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public int wayPointIndex;
+    UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(targets[wayPointIndex].transform.position);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == targets[wayPointIndex])
+        {
+            wayPointIndex = (wayPointIndex + 1) % targets.Count; //Find next waypoint in list after reaching one
+        }
+    }
+}
