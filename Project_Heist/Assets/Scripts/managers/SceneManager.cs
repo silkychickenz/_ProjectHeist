eb0e45c9146cb8f38709d86d280ea69a048a489e
Shane-sama <48694586+ShaneLogtenberg@users.noreply.github.com>
2020-12-15 16:13:05 -0500

    Made big changes to level 3
    Removed long hallway and replaced it with space!
    Added enmies just cus
    Added Deathzone script so you don't fall in space forever
    Added cover in last room
    a big more geomotry tweaking.

---
+++ Project_Heist/Assets/Scripts/managers/SceneManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace spaceHiest { 
+public class SceneManager : MonoBehaviour
+{
+        private void Start()
+        {
+            
+        }
+    }
+}
