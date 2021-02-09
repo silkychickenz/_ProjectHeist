b9601b6b32bb317498cf83330fa45c33e03bd7e4
kunal <kunalchk7@gmail.com>
2021-02-08 21:41:11 -0500

    cover system, new scene, new shoot animations, camera readjustment

---
+++ Project_Heist/Assets/Scripts/shortcuts.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class shortcuts : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    [MenuItem("Extra Tools/Clear Console %q")] // CTRL + Q
+    private static void ClearConsole()
+    {
+        Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+        Type type = assembly.GetType("UnityEditor.LogEntries");
+        MethodInfo method = type.GetMethod("Clear");
+        method?.Invoke(new object(), null);
+    }
+}
